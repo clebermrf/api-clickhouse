143fed772d2351e157e0f6833dcb578904465df5
Cleber Marschner <clebermrf@gmail.com>
2024-10-13 16:02:49 -0300

    fix: uploader

--- uploader/uploader.py
+++ uploader/uploader.py
@@ -1,54 +1,48 @@
+import logging
 import pandas as pd
 import requests
-import re
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+logger = logging.getLogger("uploader")
 
 
 class Uploader:
-
     def __init__(self, args):
-        """ Instantiate uploader given argparsers args """
+        """Instantiate uploader given argparsers args"""
 
         self.path = args.path
         self.chunksize = args.chunksize
         self.domain = args.domain
         self.port = args.port
 
-
-    @staticmethod
-    def points_formater(data):
-        """ Format actual point data for the ClickHouse point format """
-
-        rule = '\([0-9]*\.*[0-9]+ [0-9]*\.*[0-9]+\)'
-        return re.findall(rule, data)[0].replace(' ', ',')
-
-
     def upload(self, data) -> requests.Response:
-        """ Post data to the endpoint /trips """
+        """Post data to the endpoint /trips"""
 
         try:
-            response = requests.post(url=f'http://{self.domain}:{self.port}/trips', data=data)
+            response = requests.post(
+                url=f"http://{self.domain}:{self.port}/trips",
+                data=data,
+                timeout=60,
+            )
             response.raise_for_status()
 
         except requests.exceptions.HTTPError as err:
-            raise SystemExit(err)
-
+            raise SystemExit(err) from err
 
     def run(self) -> None:
-        """ Run the read and upload routine """
+        """Run the read and upload routine"""
 
         progress = 0
+        logger.info("Uploading data")
 
         for chunk in pd.read_csv(self.path, chunksize=self.chunksize):
-
-            chunk['origin_coord'] = chunk['origin_coord'] \
-                .apply(lambda x: self.points_formater(x))
-
-            chunk['destination_coord'] = chunk['destination_coord'] \
-                .apply(lambda x: self.points_formater(x))
-
-            data = chunk.to_json(orient='records')
-            self.upload(data)
+            records = chunk.to_json(orient="records")
+            self.upload(records)
 
             progress += len(chunk)
-            print(f'Lines uploaded: {progress}', end='\r')
-
+            logger.info("Lines uploaded: %s", progress)
