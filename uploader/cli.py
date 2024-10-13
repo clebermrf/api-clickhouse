7b5e7800cdce071ee6e37218b69fdd6d35e5ae9d
Cleber Marschner <clebermrf@gmail.com>
2024-10-13 16:10:22 -0300

    fix: format cli

--- uploader/cli.py
+++ uploader/cli.py
@@ -2,26 +2,26 @@ import argparse
 
 
 def run_parser() -> argparse.Namespace:
-    """ Initialize argparse """
+    """Initialize argparse"""
 
     parser = argparse.ArgumentParser(
-        description='Process args to run file uploader.'
+        description="Process args to run file uploader."
     )
     parser.add_argument(
-        '-p, --path', dest='path', required=True, type=str,
-        help='the file path to be uploaded'
+        "-p, --path", dest="path", required=True, type=str,
+        help="the file path to be uploaded"
     )
     parser.add_argument(
-        '--domain', dest='domain', type=str, default='localhost',
-        help='the domain to be uploaded'
+        "--domain", dest="domain", type=str, default="localhost",
+        help="the domain to be uploaded"
     )
     parser.add_argument(
-        '--port', dest='port', type=int, default=8080,
-        help='the port to be uploaded'
+        "--port", dest="port", type=int, default=8000,
+        help="the port to be uploaded"
     )
     parser.add_argument(
-        '--chunksize', dest='chunksize', type=int, default=1000, metavar='N',
-        help='the payload size'
+        "--chunksize", dest="chunksize", type=int, default=10, metavar="N",
+        help="the payload size"
     )
 
     return parser.parse_args()
